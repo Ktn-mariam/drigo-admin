4a0073852f858498a1fda5606fa64f1a2fcb39a5
Mariam Khatoon <mariamkhatoon996@gmail.com>
2026-09-22 20:24:34 +0530

    feat: a simple piechart added

---
+++ src/app/dashboard/FleetByCity.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { PieChart } from "@/components/PieChart"
+
+function FleetByCity() {
+  return (
+    <div className="h-96 w-full">
+      <PieChart />
+    </div>
+  )
+}
+
+export default FleetByCity
