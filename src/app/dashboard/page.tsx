a8aae86c5b70a835fd8794d5c6f37d8e42a3074b
Mariam Khatoon <mariamkhatoon996@gmail.com>
2026-09-22 17:06:37 +0530

    feat: 4 charts are now displayed

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,12 +8,18 @@ type TrendData = {
   count: number;
 }
 
+type RevenueData = {
+  date: Date;
+  revenue: number;
+}
+
 function Dashboard() {
   const router = useRouter();
   const [rentalTrends, setRentalTrends] = useState<TrendData[] | null>(null);
   const [reservationTrends, setReservationTrends] = useState<TrendData[] | null>(null);
   const [revenueTrends, setRevenueTrends] = useState<TrendData[] | null>(null);
   const [userRegistrationTrends, setUserRegistrationTrends] = useState<TrendData[] | null>(null);
+  const [labels, setLabels] = useState<string[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,10 +32,22 @@ function Dashboard() {
           router.push('/login');
         }
 
-        setRentalTrends(data.rentalTrends);
-        setReservationTrends(data.reservationTrends);
-        setRevenueTrends(data.revenueTrends);
-        setUserRegistrationTrends(data.userRegistrationTrends);
+        const labels = data.rentalTrends.map((item: TrendData) => item.date);
+        const formattedLabels = labels.map((date: string) => {
+          const day = date.substring(8, 10);
+          const month = date.substring(5, 7);
+          return `${day}/${month}`;
+        });
+        setLabels(formattedLabels);
+        const rentalData = data.rentalTrends.map((item: TrendData) => item.count);
+        const reservationData = data.reservationTrends.map((item: TrendData) => item.count);
+        const revenueData = data.revenueTrends.map((item: RevenueData) => item.revenue);
+        const userRegistrationData = data.userRegistrationTrends.map((item: TrendData) => item.count);
+
+        setRentalTrends(rentalData);
+        setReservationTrends(reservationData);
+        setRevenueTrends(revenueData);
+        setUserRegistrationTrends(userRegistrationData);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -37,32 +55,55 @@ function Dashboard() {
     fetchData();
   }, []);
 
-  const handleTrendFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const selectedFilter = event.target.value;
-    console.log('Selected trend filter:', selectedFilter);
-    // Implement logic to update the displayed trends based on the selected filter
+  const rentalDataset = {
+    label: 'Rental',
+    data: rentalTrends,
+    borderColor: 'rgba(194, 0, 251, 0.5)',
+    backgroundColor: 'rgba(194, 0, 251, 0.5)',
+    fill: true,
+    cubicInterpolationMode: 'monotone',
+  };
+
+  const reservationDataset = {
+    label: 'Reservation',
+    data: reservationTrends,
+    borderColor: 'rgba(236, 8, 104, 0.5)',
+    backgroundColor: 'rgba(236, 8, 104, 0.5)',
+    fill: true,
+    cubicInterpolationMode: 'monotone',
+  }
+
+  const revenueDataset = {
+    label: 'Revenue',
+    data: revenueTrends,
+    borderColor: 'rgba(252, 47, 0, 0.5)',
+    backgroundColor: 'rgba(252, 47, 0, 0.5)',
+    fill: true,
+    cubicInterpolationMode: 'monotone',
+  }
+
+  const userRegistrationDataset = {
+    label: 'User Registration',
+    data: userRegistrationTrends,
+    borderColor: 'rgba(252, 47, 0, 0.5)',
+    backgroundColor: 'rgba(252, 47, 0, 0.5)',
+    fill: true,
+    cubicInterpolationMode: 'monotone',
   }
 
   return (
-    <div className='flex flex-col gap-4 items-center h-96'>
-      <LineChart />
-      <div className='flex gap-4'>
-        <div className='flex gap-1 items-center'>
-          <input type="checkbox" name="Type" id="All" value="Rental" onChange={handleTrendFilterChange} />
-          <label htmlFor="All">All</label>
-        </div>
-        <div className='flex gap-1 items-center'>
-          <input type="checkbox" name="Type" id="Economy" value="Reservation" onChange={handleTrendFilterChange} />
-          <label htmlFor="Economy">Economy</label>
-        </div>
-        <div className='flex gap-1 items-center'>
-          <input type="checkbox" name="Type" id="Sedan" value="Revenue" onChange={handleTrendFilterChange} />
-          <label htmlFor="Sedan">Sedan</label>
-        </div>
-        <div className='flex gap-1 items-center'>
-          <input type="checkbox" name="Type" id="SUV" value="User Registration" onChange={handleTrendFilterChange} />
-          <label htmlFor="SUV">SUV</label>
-        </div>
+    <div className="flex flex-col gap-4 items-center w-full">
+      <div className="h-72 w-full">
+        <LineChart labels={labels} dataset={rentalDataset} />
+      </div>
+      <div className="h-72 w-full">
+        <LineChart labels={labels} dataset={reservationDataset} />
+      </div>
+      <div className="h-72 w-full">
+        <LineChart labels={labels} dataset={revenueDataset} />
+      </div>
+      <div className="h-72 w-full">
+        <LineChart labels={labels} dataset={userRegistrationDataset} />
       </div>
     </div>
   )
